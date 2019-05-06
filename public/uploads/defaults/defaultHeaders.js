1edbecd98a89f84937ed16c649d622645296b842
Stephen <ridingjr@gmail.com>
2019-05-05 19:23:38 -0700

    lets try deploying to heroku and see how this works out.

--- public/uploads/defaults/defaultHeaders.js
+++ public/uploads/defaults/defaultHeaders.js
@@ -1,6 +1,6 @@
 const defaultHeaders = {
-  0: "aaron-burden-363695-unsplash.jpg",
-  1: "aron-van-de-pol-115112-unsplash.jpg",
+  0: "black-bg.jpg",
+  1: "secondary-bg.jpg",
   2: "jeremy-bishop-335002-unsplash.jpg",
   3: "jonathan-riley-118591-unsplash.jpg",
   4: "nasa-89127-unsplash.jpg",
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -10,7 +11,7 @@ const postRoute = require("./routes/api/post.js");
 const profileRoute = require("./routes/api/profile.js");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //Uses bodyparser to parse incoming request bodies and make info available under req.body
 app.use(bodyParser.json());
@@ -18,10 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
@@ -39,15 +37,19 @@ app.listen(port, () => {
 
 app.use(express.static("public"));
 
-//Test get route
-app.get("/", (req, res) => {
-  res.send(`This message was sent through port ${port} at /`);
-});
-
 //Route to make calls to user
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
 app.use("/api/profile", profileRoute);
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //Connecting to mongodb database
 mongoose
