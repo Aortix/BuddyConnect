0d54b7cc9630743db2369d5124a92ba3810584e0
Stephen <ridingjr@gmail.com>
2019-07-05 09:35:15 -0700

    removed redux chrome extension config

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
 const initialState = {};
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [thunk];
 
 export default function store() {
-  return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
+  return createStore(rootReducer, initialState, applyMiddleware(...middleware));
 }
